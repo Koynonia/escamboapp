e354dfcb1a574649feca70d67bd49973a43ac454
koynonia <koynonia@gmail.com>
2018-09-27 16:49:32 -0300

    022 - Cadastro de Categorias Backoffice (Parte 2 de 5)

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -2,4 +2,20 @@ class Backoffice::CategoriesController < BackofficeController
 	def index
 		@categories = Category.all
 	end
+
+	def create
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
 end
