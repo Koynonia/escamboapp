7cf6b866ac8adf90c73bc359815a53c918182f61
koynonia <koynonia@gmail.com>
2018-10-03 13:33:04 -0300

    042 - Devise Errors & Collection Radio Buttons

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -4,16 +4,16 @@ class Backoffice::AdminsController < BackofficeController
 	after_action :verify_policy_scoped, only: :index
 
 	def index
-		#@admin = Admin.all
-		#@admin = Admin.with_restricted_access
-		@admin = policy_scope(Admin)
+		#@admins = Admin.all
+		#@admins = Admin.with_restricted_access
+		@admins = policy_scope(Admin)
 	end
 
 	def create
 		@admin = Admin.new(params_admin)
 		if @admin.save
 			redirect_to  backoffice_admins_path, 
-			notice: I18n.t('messages.created_with', item: @admin.name)
+			notice: I18n.t('messages.created', item: @admin.name)
 		else
 			render :new
 		end
@@ -30,7 +30,7 @@ class Backoffice::AdminsController < BackofficeController
 	def update
 		if @admin.update(params_admin)
 			redirect_to  backoffice_admins_path, 
-			notice:  I18n.t('messages.updated_with', item: @admin.name)
+			notice:  I18n.t('messages.updated', item: @admin.name)
 		else
 			render :edit
 		end
