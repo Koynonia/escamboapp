a260d54f6149d2b1a141b7bff4c00dc63e19cb90
koynonia <koynonia@gmail.com>
2018-11-09 20:48:39 -0200

    105 - Validando Nested Attributes e Validações Personalizadas

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,8 +8,24 @@ class Member < ActiveRecord::Base
 	has_one :profile_member
 	accepts_nested_attributes_for :profile_member
 
+	#Validations
+	validate :nested_attributes
+
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 	devise :database_authenticatable, :registerable,
 	:recoverable, :rememberable, :validatable
+
+	private
+
+	def nested_attributes
+		if nested_attributes_blank?
+			errors.add( :base, I18n.t('messages.error_nested_attributes') )
+		end
+	end
+
+	def nested_attributes_blank?
+		profile_member.first_name.blank? || profile_member.second_name.blank?
+	end
+
 end
