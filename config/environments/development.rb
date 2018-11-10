d10bcd497ef65e79122de462164200432ed29696
koynonia <koynonia@gmail.com>
2018-11-09 22:53:40 -0200

    107 - Recuperando a senha com o Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.web_console.whiny_requests = false
 
   # Devise Config:
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Mailcatcher Config:
   config.action_mailer.delivery_method = :smtp
