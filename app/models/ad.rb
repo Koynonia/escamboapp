e4f50075f4ed3961dad6002bd38d42469e2374a8
Fernando Moraes Oliveira <koynonia@gmail.com>
2018-11-23 18:33:26 -0200

    Ajustando a aplicação em modo de desenvolvimento.

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -39,7 +39,7 @@ class Ad < ActiveRecord::Base
    }
 
   # paperclip
-  has_attached_file :picture, styles: { large: "800x300#", medium: "320x150#", thumb: "100x100>" }, 
+  has_attached_file :picture, styles: { large: "900x350#", medium: "700x400#", thumb: "100x100>" }, 
   default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
