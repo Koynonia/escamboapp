a089a79965e1b5792c755ba2ba073d02f5ae3682
koynonia <koynonia@gmail.com>
2018-12-13 12:18:03 -0200

    014 - Conhecendo os Cookies

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,7 +1,16 @@
 class Site::HomeController < SiteController
   def index
+  	cookies[:user_name] = "David"
+  	cookies[:lat_long] = JSON.generate([47.68, -122.37])
+  	cookies[:login] = { value: "XJ-122", expires: 1.hour.from_now }
+  	cookies.signed[:user_id] = "Fernando M. Oliveira"
+  	cookies.encrypted[:discount] = "EscamboApp"
+
   	@categories = Category.order_by_description
   	@ads = Ad.descending_order(params[:page])
   	@carousel = Ad.random(3)
+
+  	puts "=========================== >> #{cookies[:user_name]}"
+  	puts "=========================== >> #{eval(cookies[:lat_long]).class}"
   end
 end
--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -3,5 +3,9 @@ class Site::SearchController < SiteController
 	def ads
 		@ads = Ad.search(params[:q], params[:page])
 		@categories = Category.all
+
+		cookies[:search_term] = params[:q]
+		cookies[:categories] = @categories.to_json
+		puts "=========================== >> #{cookies[:categories]}"
 	end
 end
