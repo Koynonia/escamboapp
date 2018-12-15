93f2c677ac018fc9fedc07de77c9ffcc76bc04be
koynonia <koynonia@gmail.com>
2018-12-15 15:53:18 -0200

    015 - Sessions

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,5 +1,9 @@
 class Site::HomeController < SiteController
   def index
+  	# Exemplo do uso do cookie da Session
+  	session[:name] = "Fernando"
+  	puts "================= >> #{session[:name]}"
+
   	@categories = Category.order_by_description
   	@ads = Ad.descending_order(params[:page])
   	@carousel = Ad.random(3)
